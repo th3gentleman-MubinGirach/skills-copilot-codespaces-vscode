c55e7c4e3039bab2ad80d818650a527929a41902
th3gentleman-MubinGirach <girachmubin@gmail.com>
2023-08-10 09:20:16 +0000

    member.js

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 34,
+        calculateNumbers: function(var1, var2) {
+            return var1 + var2;
+        }
+    }
+    return member;
